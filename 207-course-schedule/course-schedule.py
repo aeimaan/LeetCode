6adbe58d0fb5790eabe43337bb75029e3a60f5dc
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-02-16 20:15:37 -0500

    Time: 5 ms (60.99%) | Memory: 19.6 MB (23.17%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = { i:[] for i in range(numCourses) }
+        visited = set()
+
+        for course, prereq in prerequisites:
+            adj_list[course].append(prereq)
+        
+        def topSort(node):
+            if adj_list[node] == []: # no dependencies
+                return True
+            if node in visited:     # Circular dependency
+                return False
+
+            # Node not seen, and has neighbors -> Now do stuff
+            visited.add(node)
+            for prereq in adj_list[node]:
+                if topSort(prereq) == False:
+                    return False
+            visited.remove(node)
+            adj_list[node] = []
+            return True
+        
+        for i in range(numCourses):
+            if i not in visited:
+                if topSort(i) == False:
+                    return False
+        
+        return True
+
+        
