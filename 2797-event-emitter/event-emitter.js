6519888c1fd9ec84f56e2486bf848466cf205f40
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-02-21 14:21:21 -0500

    Time: 52 ms (71.47%) | Memory: 53.9 MB (8.26%) - LeetSync

--- 2797-event-emitter/event-emitter.js
+++ 2797-event-emitter/event-emitter.js
@@ -1,83 +1,62 @@
 class EventEmitter {
-  constructor() {
-    // Map from event name -> array of callbacks
-    this.events = new Map();
-    // Global list of all subscriptions in chronological order
-    this.subscriptions = [];
-  }
-
-  /**
-   * @param {string} eventName
-   * @param {Function} callback
-   * @return {Object} with { unsubscribe() }
-   */
-  subscribe(eventName, callback) {
-    // 1) Add callback to the event's array
-    if (!this.events.has(eventName)) {
-      this.events.set(eventName, []);
+    constructor(){
+        this.events = new Map();
+        this.subscriptions = [];
     }
-    const arr = this.events.get(eventName);
-    arr.push(callback);
-
-    // 2) Add to global subscriptions
-    const index = this.subscriptions.length;
-    this.subscriptions.push({ eventName, callback });
-
-    // 3) Return an object with unsubscribe
-    return {
-      unsubscribe: () => {
-        // remove from the event array
-        const arr = this.events.get(eventName);
-        if (!arr) return undefined;
-        const idx = arr.indexOf(callback);
-        if (idx !== -1) {
-          arr.splice(idx, 1);
+    /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+    subscribe(eventName, callback) {
+        if (!this.events.has(eventName)){ // event has no subscribers
+            this.events.set(eventName, []);
         }
-        // mark global subscription as removed
-        this.subscriptions[index] = null;
-        return undefined;
-      }
-    };
-  }
+        const arr = this.events.get(eventName);
+        arr.push(callback);
 
-  /**
-   * Unsubscribe by global index
-   * @param {number} globalIndex
-   * @return {undefined}
-   */
-  unsubscribe(globalIndex) {
-    const sub = this.subscriptions[globalIndex];
-    if (!sub) {
-      return undefined; // already unsubscribed or invalid
+        // const subscriptionIndex = this.subscriptions.length;
+        // this.subscriptions.push({eventName, callback})
+        return {
+            unsubscribe: () => {
+                const arr = this.events.get(eventName);
+                if (!arr) return;
+                const idx = arr.indexOf(callback)
+                if (idx !== -1){ //found 
+                    arr.splice(idx, 1);
+                }
+                // this.subscriptions[subscriptionIndex] = null;
+                return undefined;
+            }
+        };
     }
-    const { eventName, callback } = sub;
-    // remove from the event array
-    const arr = this.events.get(eventName);
-    if (arr) {
-      const idx = arr.indexOf(callback);
-      if (idx !== -1) {
-        arr.splice(idx, 1);
-      }
+    
+    /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+    emit(eventName, args = []) {
+        const arr = this.events.get(eventName);
+        if (!arr){
+            return [];
+        }
+        const results = []
+        for (const cb of arr){
+            results.push(cb(...args)) //call the call backs with args, then push onto res
+        }
+        return results;
     }
-    // mark it unsubscribed
-    this.subscriptions[globalIndex] = null;
-    return undefined;
-  }
-
-  /**
-   * @param {string} eventName
-   * @param {Array} args
-   * @return {Array}
-   */
-  emit(eventName, args = []) {
-    const arr = this.events.get(eventName);
-    if (!arr) {
-      return [];
-    }
-    const results = [];
-    for (const cb of arr) {
-      results.push(cb(...args));
-    }
-    return results;
-  }
 }
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
