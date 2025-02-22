694bf7598fd61700fea333bfeb8459311b8341ce
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-02-22 16:16:26 -0500

    Time: 4 ms (75.70%) | Memory: 17.7 MB (97.46%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,24 +1,21 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        if len(s) == 0 or len(s) < k : return 0     #nothing can be made here
-        if k <= 1: return len(s)                    #everything valid
+        if len(s) == 0 or len(s)<k:
+            return 0
+        if k == 1: return len(s)
 
         fmap = {}
-
-        for c in s:
-            fmap[c] = 1 + fmap.get(c, 0)
-
         l = 0
-        while l < len(s) and fmap[s[l]] >= k:
-            l += 1
-        
-        if l >= len(s)-1:       #Whole string is valid
-            print(len(s) , l)
-            return l
+        for c in s:
+            fmap[c] = fmap.get(c, 0) + 1
 
-        left_str = self.longestSubstring( s[0:l], k )
-        while l<len(s) and fmap[s[l]] < k: l += 1
-        right_str = 0
-        if l < len(s):
-            right_str = self.longestSubstring(s[l:], k)
+        while l < len(s) and fmap[s[l]]>= k:
+            l += 1
+
+        if l >= len(s) - 1:
+            return l
+        
+        left_str = self.longestSubstring( s[0:l] , k)
+        while l < len(s) and fmap[s[l]] < k: l += 1
+        right_str = self.longestSubstring(s[l:], k)
         return max(left_str, right_str)
