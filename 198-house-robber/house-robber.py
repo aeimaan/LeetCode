ff0e637a8790f0bd09b608d988443e15e48c904a
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-02-24 11:01:52 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.10%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2: return max(nums)
+        if len(nums) <= 1: return max(nums)
 
-        mem = [0] * len(nums)
-        maxMoney = max(nums[0], nums[1])
-        mem[1] = maxMoney
-        mem[0] = nums[0]
-        
-        for i in range(2, len(nums)):
-            num = nums[i]
-            mem[i] = max( num+mem[i-2], mem[i-1] )
-        return mem[-1]
+        memory = [0] * len(nums)
+        memory[0] = nums[0] 
+        memory[1] = max(nums[0], nums[1])
+
+        for i in range(2,len(nums)):
+            # can either rob current house pluse i - 2
+            # or not rob current house so continue i-1
+            # Choose by max
+            memory[i] = max(nums[i] + memory[i-2], memory[i-1])
+
+        return memory[-1]
