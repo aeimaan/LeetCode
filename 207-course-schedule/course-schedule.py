854b7ffa817350a51115c951851a8a2e66028fac
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-01 17:43:23 -0500

    Time: 3 ms (89.09%) | Memory: 20 MB (8.39%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,32 +1,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = { i:[] for i in range(numCourses) }
+        # Want to detect that there are no cycles in ths graph
+
+        adj_list = { i:[]  for i in range(numCourses)}
+        cycle = set()
         visited = set()
 
         for course, prereq in prerequisites:
             adj_list[course].append(prereq)
         
-        def topSort(node):
-            if adj_list[node] == []: # no dependencies
-                return True
-            if node in visited:     # Circular dependency
+        def dfs(node):
+            if node in cycle:
                 return False
+            if node in visited:
+                return True
 
-            # Node not seen, and has neighbors -> Now do stuff
-            visited.add(node)
+            cycle.add(node)
             for prereq in adj_list[node]:
-                if topSort(prereq) == False:
+                if dfs(prereq) == False:
                     return False
-            # Remove so that other nodes dont trigger false for a node that was actually true
-            # visited.remove(node)
-            adj_list[node] = []
+            
+            cycle.remove(node)
+            visited.add(node)
             return True
         
         for i in range(numCourses):
-            if i not in visited:
-                if topSort(i) == False:
-                    return False
-        
+            if dfs(i) == False:
+                return False
         return True
-
-        
