1201511682a2813f30fbefa55331d9f200e24176
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-01 15:38:12 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (23.10%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "" : return []
+        lettersFromNum = {'2': ['a','b','c'], '3':['d','e','f'] , '4':['g','h','i'] , 
+        '5':['j','k','l'], '6':['m','n','o'] , '7':['p','q','r', 's'] , '8':['t','u','v'], '9':['w','x','y', 'z']}
+
+        path = ""
+        res = []
+
+        def dfs ( idx, path):
+            if idx == len(digits):
+                res.append(path)
+                return
+            
+            neighbors = lettersFromNum[digits[idx]]
+
+            for char in neighbors:
+                path += (char)
+                dfs(idx + 1, path)
+                path = path[:-1]
+
+            return 
+        dfs(0, path)
+        return res
