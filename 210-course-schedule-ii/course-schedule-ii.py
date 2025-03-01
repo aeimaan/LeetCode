c99380392dd3423168ad40b7d75a33b5baaaa01c
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-01 17:34:48 -0500

    Time: 3 ms (77.96%) | Memory: 19.8 MB (22.17%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj_list = {i:[] for i in range(numCourses)}
+
+        for course, prereq in prerequisites:
+            adj_list[course].append(prereq)
+
+        res = []
+        visited = set()
+        cycle = set()
+
+        def dfs(node):
+            if node in cycle:
+                return False
+            if node in visited:
+                return True
+            if adj_list[node] == []:
+                res.append(node)
+                visited.add(node)
+                return True
+            
+            cycle.add(node)
+            for pre in adj_list[node]:
+                if dfs(pre) == False:
+                    return False
+            cycle.remove(node)
+            visited.add(node)
+            res.append(node)
+            return True
+        
+        for i in range(numCourses):
+            if i not in visited:
+                if dfs(i) == False:
+                    return []
+        
+        return res
+        
