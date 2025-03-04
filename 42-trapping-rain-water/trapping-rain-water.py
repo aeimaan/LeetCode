c806336cbf2abda9445fba2d062f54a6c6691c8a
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-04 00:03:11 -0500

    Time: 26 ms (31.60%) | Memory: 19.4 MB (40.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,35 +1,27 @@
 class Solution:
-
-
-
     def trap(self, height: List[int]) -> int:
         max_left = [0] * len(height)
         max_right = [0] * len(height)
-        water = 0
-
+        res = 0
         max_wall(height, max_left, 0, len(height))
-        max_wall(height, max_right, len(height)-1, 0)
-
+        max_wall(height, max_right, len(height)-1, -1)
         for i in range(len(height)):
-            cur_height = height[i]
-            min_wall = min(max_left[i], max_right[i])
-            if cur_height >= min_wall:      ## cannot trap anything
-                continue
-            
-            water += (min_wall - cur_height)
-    
-        return water
+            minH = min(max_left[i], max_right[i])
+            if height[i] < minH:
+                res += minH - height[i]
+        
+        return res
+
 
 def max_wall(height, arr, start, end):
-        max_before = 0
-        if start<end:
-            for i in range(start, end):
-                arr[i] = max_before
-                max_before = max(max_before, height[i])
-        else:
-           for i in range(start, end, -1):
-                arr[i] = max_before
-                max_before = max(max_before, height[i]) 
-
-
+    curMax = 0
+    if start < end:
+        for i in range(start, end):
+            arr[i] = curMax
+            curMax = max(curMax, height[i])
+    else:
+        for i in range(start, end, -1):
+            arr[i] = curMax
+            curMax = max(curMax, height[i])
     
+        
