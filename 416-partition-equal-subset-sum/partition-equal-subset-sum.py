cca57d6216faed48349669d8058ff27420a79e38
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-07 22:12:02 -0500

    Time: 2259 ms (13.28%) | Memory: 483.3 MB (7.12%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -8,11 +8,11 @@ class Solution:
 
         def dfs(idx, val):
             if val == target: return True
-            if (idx, val) in memo or val > target: return False
-            if idx >= len(nums): return False
+            if (idx, val) in memo: return memo[(idx, val)]
+            if idx >= len(nums) or val > target: return False
 
             options = dfs(idx+1, val) or dfs(idx + 1, val + nums[idx])
-            memo[(idx, val)] = 0
+            memo[(idx, val)] = options
             return options
         return dfs(0, 0)
             
