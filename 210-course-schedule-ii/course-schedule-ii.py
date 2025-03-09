076f6a3d4ed4f082659b2e62382640ef1018e644
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-09 17:53:08 -0400

    Time: 4 ms (51.08%) | Memory: 19.9 MB (13.18%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,27 +1,20 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj_list = {i:[] for i in range(numCourses)}
-
-        for course, prereq in prerequisites:
-            adj_list[course].append(prereq)
-
-        res = []
+        adj_list = { i:[] for i in range(numCourses)}
         visited = set()
         cycle = set()
+        res = []
 
+        for course, pre in prerequisites:
+            adj_list[course].append(pre)
+        
         def dfs(node):
-            if node in cycle:
-                return False
-            if node in visited:
-                return True
-            # if adj_list[node] == []:
-            #     res.append(node)
-            #     visited.add(node)
-            #     return True
-            
+            if node in cycle: return False
+            if node in visited: return 
+
             cycle.add(node)
-            for pre in adj_list[node]:
-                if dfs(pre) == False:
+            for neigh in adj_list[node]:
+                if dfs(neigh) == False:
                     return False
             cycle.remove(node)
             visited.add(node)
@@ -29,9 +22,65 @@ class Solution:
             return True
         
         for i in range(numCourses):
-            if i not in visited:
-                if dfs(i) == False:
-                    return []
-        
+            if dfs(i) == False:
+                return []
         return res
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # adj_list = {i:[] for i in range(numCourses)}
+        # for course, prereq in prerequisites:
+        #     adj_list[course].append(prereq)
+
+        # res = []
+        # visited = set()
+        # cycle = set()
+
+        # def dfs(node):
+        #     if node in cycle:
+        #         return False
+        #     if node in visited:
+        #         return True
+        #     if adj_list[node] == []:
+        #         res.append(node)
+        #         visited.add(node)
+        #         return True
+            
+        #     cycle.add(node)
+        #     for pre in adj_list[node]:
+        #         if dfs(pre) == False:
+        #             return False
+        #     cycle.remove(node)
+        #     visited.add(node)
+        #     res.append(node)
+        #     return True
+        
+        # for i in range(numCourses):
+        #     if i not in visited:
+        #         if dfs(i) == False:
+        #             return []
+        
+        # return res
         
