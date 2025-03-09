0795e5d95bd7f9da1028062d94c94d3e3a6ee385
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-09 18:26:32 -0400

    Time: 2 ms (51.81%) | Memory: 19 MB (65.36%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,24 +1,24 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) > (n - 1):
-            return False
+        if len(edges) > n-1: return False
+        adj_list = { i:[] for i in range(n)}
+        visited= set()
+        # Want no cycles
+
+        for x,y in edges:
+            adj_list[x].append(y)
+            adj_list[y].append(x)
         
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
-        
-        visit = set()
-        def dfs(node, par):
-            if node in visit:
+        def dfs(node, parent):
+            if node in visited:
                 return False
-            
-            visit.add(node)
-            for nei in adj[node]:
-                if nei == par:
+            visited.add(node)
+            for x in adj_list[node]:
+                if x == parent:
                     continue
-                if not dfs(nei, node):
+                if dfs(x, node) == False:
                     return False
-            return True
-        
-        return dfs(0, -1) and len(visit) == n
+            
+
+        if dfs(0,-1) == False: return False
+        return True if len(visited) == n else False
