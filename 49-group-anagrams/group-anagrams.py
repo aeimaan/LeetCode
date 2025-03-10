1a3205998e803d35cc7119082d1be314d28302bc
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-03-09 22:01:48 -0400

    Time: 13 ms (56.90%) | Memory: 20.5 MB (89.52%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # create a fmap of the str[i]
+        # use that fmap as a key to look in the result array and append
+
+        # or sort each str[i] and then sort as items will be the same
+
+
+        res = {}
+
+        for s in strs:
+            tmp = ''.join(sorted(s))
+            if tmp not in res:
+                res[tmp] = []
+            res[tmp].append(s)
+        return list(res.values())
