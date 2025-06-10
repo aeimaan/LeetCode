4b681f1b56b3a0102d9d6f07b7fdee1fc1be5007
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-06-09 21:28:31 -0700

    Time: 20 ms (5.31%) | Memory: 28.5 MB (7.79%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        fmap = {}
+        highest = 0
+        for x in nums:
+            fmap[x] = fmap.get(x, 0) + 1
+            highest = max(highest, fmap[x])
+        
+        fmap2 = {i+1:[] for i in range(len(nums))} 
+        print(fmap2)
+        for num, count in fmap.items():
+            fmap2[count].append(num)
+
+        res = []
+
+        for i in range(highest, -1, -1):
+            for x in fmap2[i]:
+                k -= 1
+                res.append(x)
+                if k == 0: return res
+        
+        return res
+
