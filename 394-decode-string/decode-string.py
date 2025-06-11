7682517e124e3880bb619dd4d94a08c7f56b4ec7
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-06-10 20:02:44 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.90%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            if c != "]":
+                stack.append(c)
+            else:
+                # found a closing bracket
+                # now pop until u hit a [
+                word = ""
+                tmp = stack.pop(-1)
+                while tmp != "[" and stack:
+                    word += tmp
+                    tmp = stack.pop(-1)
+                word = word[::-1]
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num += stack.pop(-1)
+                num = num[::-1]
+                for i in range(int(num)):
+                    for c in word:
+                        stack.append(c)
+                
+        return ''.join(stack)
