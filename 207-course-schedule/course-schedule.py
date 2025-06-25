c22be6197462597b7b80377d5968a24b541dae73
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-06-24 20:29:06 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (21.77%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,24 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = {i:[] for i in range(numCourses)}
+        adj_list = { i:[] for i in range(numCourses) }
         cycle = set()
-        finished = set()
 
         for course, pre in prerequisites:
             adj_list[course].append(pre)
-
-        def dfs(course):
-            if course in cycle: return False
-            if course in finished: return True
-
-            cycle.add(course)
-            for pre in adj_list[course]:
-                if dfs(pre) == False:
-                    return False
-            cycle.remove(course)
-            finished.add(course)
+        
+        def dfs(node):
+            if node in cycle: return False
+            if adj_list[node] == []: return True
+            cycle.add(node)
+            for pre in adj_list[node]:
+                if not dfs(pre): return False
+            cycle.remove(node)
+            adj_list[node] = []
             return True
         
         for i in range(numCourses):
             if not dfs(i): return False
-        return True  
+        
+        return True
