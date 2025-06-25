3518b85a5760c0e7a000df22ee2d46bc9fbd9b3e
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-06-24 21:18:16 -0700

    Time: 107 ms (18.43%) | Memory: 28.7 MB (70.69%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,18 +1,15 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         fmap = {}
-        maxNum = nums[0]
-        minNum = nums[0]
+        minn = nums[0]
+        maxx = nums[0]
+        count = 0
         for x in nums:
-            fmap[x] = 1+ fmap.get(x, 0)
-            maxNum = max(maxNum, x)
-            minNum = min(minNum, x)
-
-        if minNum == maxNum: return maxNum
-
-        for i in range(maxNum, minNum-1, -1):
+            fmap[x] = fmap.get(x, 0) + 1
+            minn = min(minn, x)
+            maxx = max(maxx, x)
+        if maxx == minn: return maxx
+        for i in range(maxx, minn-1, -1):
             if i in fmap:
-                k -= fmap[i]
-                if k <= 0:
-                    return i
-        
+                count += fmap[i]
+                if k <= count: return i
