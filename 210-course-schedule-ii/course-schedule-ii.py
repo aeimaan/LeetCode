433420637ad41f785282801e531133657e67bd74
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-06-24 20:36:46 -0700

    Time: 2 ms (81.69%) | Memory: 19.8 MB (12.52%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -3,24 +3,28 @@ class Solution:
         adj_list = { i:[] for i in range(numCourses) }
         cycle = set()
         res = []
+        finished = set()
 
         for course, pre in prerequisites:
             adj_list[course].append(pre)
         
         def dfs(node):
             if node in cycle: return False
+            if node in finished: return True
             if adj_list[node] == []:
-                if node not in res: res.append(node)
+                res.append(node)
+                finished.add(node)
                 return True
             cycle.add(node)
             for pre in adj_list[node]:
                 if not dfs(pre): return False
             cycle.remove(node)
             adj_list[node] = []
-            if node not in res: res.append(node)
+            res.append(node)
+            finished.add(node)
             return True
         
         for i in range(numCourses):
             if not dfs(i): return []
         
-        return list(res)
+        return res
