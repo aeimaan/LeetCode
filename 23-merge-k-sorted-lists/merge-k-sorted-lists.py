dc73699f15a21b00d8819f38b1ced4148d98b765
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-07-13 20:34:42 -0700

    Time: 10 ms (66.00%) | Memory: 20.4 MB (36.62%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -6,16 +6,26 @@
 import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        '''
+        Create a heap
+        push all the head nodes from lists onto the min heap
+        create a dummy node to indicate start of list. and a tail pointer
+        then while the heap exists, pop from it the top node.
+        make the tail.next point to that node
+        tail = tail.next
+        then for node.next (if exists) push back onto the heap.
+        repeat while the heap exists
+        '''
         heap = []
 
         for head in lists:
-            if head: heappush(heap, (head.val,id(head), head))
-
-        dummy = ListNode(0)
+            if head:
+                heappush(heap, (head.val, id(head), head))
+        
+        dummy = ListNode()
         tail = dummy
-
-        while heap: 
-            val, _, node= heappop(heap)
+        while heap:
+            val, addy, node = heappop(heap)
             tail.next = node
             tail = tail.next
             node = node.next
