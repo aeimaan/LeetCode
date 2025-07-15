5b98e32ec81e4717bf746f678fddeca576af7637
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-07-14 20:38:22 -0700

    Time: 283 ms (20.79%) | Memory: 25.6 MB (15.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,22 +5,19 @@ class Solution:
         COLS = len(grid[0])
         res = 0
 
-        def dfs(x,y,visited):
-            if x < 0 or x >= ROWS or y<0 or y>=COLS:
-                return
-            if (x,y) in visited or grid[x][y] == "0":
-                return
+        def dfs(x,y):
+            if x < 0 or x >= ROWS or y < 0 or y >= COLS: return
+            if grid[x][y] == "0" or (x,y) in visited: return
 
             visited.add((x,y))
-            dirs = [ [1,0], [0,1], [-1,0], [0,-1] ]
-
-            for _x, _y in dirs:
-                dfs(x + _x, y + _y, visited)
+            dirs = [[1,0] , [0,1] ,[-1,0], [0,-1]]
+            for dx,dy in dirs:
+                dfs(dx+x, dy+y)
+            
             return
-
-        for x in range(ROWS):
-            for y in range(COLS):
-                if grid[x][y] == "1" and (x,y) not in visited:
-                    dfs(x,y,visited)
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    dfs(i,j)
                     res += 1
         return res
