84f0cbd9e0228468f3775fe7cdd5b4d311973618
aeimaan <77698839+aeimaan@users.noreply.github.com>
2025-07-14 21:54:43 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (55.10%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -3,23 +3,21 @@ class Solution:
         stack = []
 
         for c in s:
-            if c != "]":
+            if c != ']':
                 stack.append(c)
             else:
-                # found a closing bracket
-                # now pop until u hit a [
+                # Gather backwards
                 word = ""
                 tmp = stack.pop(-1)
-                while tmp != "[" and stack:
+                while tmp != '[':
                     word += tmp
                     tmp = stack.pop(-1)
+                # Reverse the word
                 word = word[::-1]
                 num = ""
-                while stack and stack[-1].isdigit():
+                while stack and stack[-1].isnumeric():
                     num += stack.pop(-1)
                 num = num[::-1]
                 for i in range(int(num)):
-                    for c in word:
-                        stack.append(c)
-                
-        return ''.join(stack)
+                    for c in word: stack.append(c)
+        return "".join(stack)
